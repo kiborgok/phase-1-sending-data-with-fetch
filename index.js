cb0799e1921b26769d2f20ee2283115b376545f8
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-15 10:10:34 +0300

    submit a fetch post request

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector("body").innerHTML = data["id"];
+    })
+    .catch((error) => {
+      document.querySelector("body").innerHTML = error.message;
+    });
+}
